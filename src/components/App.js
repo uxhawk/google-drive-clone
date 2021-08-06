bd72c974bffed7554e767cafd3c92c059d71dab3
uxhawk <uxhawk@gmail.com>
2021-08-06 08:08:07 +0200

    moved auth components into auth folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Signup from './Signup';
+import Signup from './authentication/Signup';
 import Dashboard from './Dashboard';
-import Login from './Login';
+import Login from '././authentication//Login';
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from '../contexts/AuthContext';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import PrivateRoute from './PrivateRoute';
-import ForgotPassword from './ForgotPassword';
-import UpdateProfile from './UpdateProfile';
+import PrivateRoute from './authentication/PrivateRoute';
+import ForgotPassword from './authentication/ForgotPassword';
+import UpdateProfile from '././authentication/UpdateProfile';
 
 function App() {
   return (
