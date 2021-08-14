7979201d84df3aecf39f7e46122dfb6da8832fe5
uxhawk <uxhawk@gmail.com>
2021-08-14 23:18:12 +0200

    updated profile to blueprint

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,46 +1,33 @@
-import React, { useState } from 'react';
-import { Card, Button, Alert } from 'react-bootstrap';
+import React, {  } from 'react';
+// import { Button } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext';
 import { Link } from 'react-router-dom';
 import CenteredContainer from './authentication/CenteredContainer';
 import Navbar from '../components/google-drive/Navbar'
+import '@blueprintjs/core/lib/css/blueprint.css';
+import { Card, Elevation, Button, Icon, IconSize } from '@blueprintjs/core';
+
 
 export default function Dashboard() {
-    const [error, setError] = useState('');
-    const { currentUser, logout } = useAuth();
-    
-    async function handleLogout() {
-        setError('');
-
-        try {
-            await logout();
-        } catch {
-            setError('Failed to sign out');
-        }
-    }
+    const { currentUser } = useAuth();
     
     return (
         <>
         <Navbar />
         <CenteredContainer>
-            <Card>
-                <Card.Body>
-                    <h2 className='text-center mb-4'>Profile</h2>
-                    {error && <Alert variant='danger'>{error}</Alert>}
-                    <div className='mb-4'>
-                        <strong>Username: </strong> {currentUser.displayName}
+            <Card elevation={Elevation.THREE}> 
+                <div className='profile-edit'>
+                    <div style={{display: 'inherit', alignItems: 'center'}}>
+                        <Icon icon="user" size={40} style={{marginRight: '10px'}}  />
+                        <h2 className='bp3-heading'>Profile</h2>
                     </div>
-                    <div className='mb-2'>
-                        <strong>Email: </strong> {currentUser.email}
-                    </div>
-                    
-
-                    <Link to='/update-profile' className='btn btn-primary w-100 mt-3'>Update Profile</Link>
-                </Card.Body>
+                    <Link to='/update-profile' className='button-link'>
+                        <Button className="bp3-minimal" icon="edit" />
+                    </Link>
+                </div>
+                <p><strong>Username: </strong> {currentUser.displayName}</p>
+                <p><strong>Email: </strong> {currentUser.email}</p>
             </Card>
-            <div className='w-100 text-center mt-2'>
-            <Button variant='link' onClick={handleLogout}>Sign Out</Button>
-            </div> 
         </CenteredContainer>
         </>
     )
