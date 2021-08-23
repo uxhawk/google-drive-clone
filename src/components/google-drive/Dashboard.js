d51d91959c7006f68f51aa7509b88383a5a012fa
uxhawk <uxhawk@gmail.com>
2021-08-23 10:04:45 +0200

    removed folders section title

--- src/components/google-drive/Dashboard.js
+++ src/components/google-drive/Dashboard.js
@@ -28,14 +28,6 @@ export default function Dashboard() {
           </div>
 
           {/* print child folders */}
-          {folder.name === "Home" && childFolders.length > 0 ? (
-            <h3 className={"bp3-heading"} style={{ marginBottom: "10px" }}>
-              Folders
-            </h3>
-          ) : (
-            <></>
-          )}
-
           {childFolders.length > 0 && (
             <div className="d-flex flex-wrap">
               {childFolders.map((childFolder) => (
